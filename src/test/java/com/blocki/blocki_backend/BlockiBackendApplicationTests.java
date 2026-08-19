c6ef175f42499ca278cf68d94cbd1ca05140ad99
Woorim <yiurimone@gmail.com>
2026-08-19 15:26:55 +0900

    chore: initial spring boot setup for Blocki backend

---
+++ src/test/java/com/blocki/blocki_backend/BlockiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blocki.blocki_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
